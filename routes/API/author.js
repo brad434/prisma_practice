baa778f58e7f47e06c81102f0e7cf8d7eeb83224
Bradley Chery <Bradley434@hotmail.com>
2024-06-24 21:55:20 -0400

    added a Delete and Put route for Author file

--- routes/API/author.js
+++ routes/API/author.js
@@ -13,4 +13,46 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const authorId = parseInt(req.params.id);
+    await prisma.post.deleteMany({
+      where: {
+        authorId: authorId,
+      },
+    });
+    await prisma.author.delete({
+      where: {
+        id: authorId,
+      },
+    });
+    console.log("Author has been deleted.");
+    res.status(200).json({ message: "Author has been deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "An error occured while deleting the author." });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const authorId = parseInt(req.params.id);
+    const { name, email } = req.body;
+
+    const updatedAuthor = await prisma.author.update({
+      where: {
+        id: authorId,
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+    res.status(200).json(updatedAuthor);
+  } catch (error) {
+    console.error(error);
+  }
+});
 module.exports = router;
